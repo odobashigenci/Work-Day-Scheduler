abb7e9a0bb97db4c6ef681b25a99a9fb2c5a4dd8
odobashigenci <121208981+odobashigenci@users.noreply.github.com>
2023-02-27 23:43:40 -0500

    updating final code

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,37 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-$(function () {
-  });
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function?
-    $('.saveBtn').click(function(){
-      var userInput = $(this).siblings('.description').val();
-      var time = $(this).parent().attr('value');
-      localStorage.setItem(userInput, time);
-    })
-    
-    // How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    $(".time-block").each(function (input) {
-      var now = parseInt(dayjs().format('H'));
-      var timeX = $(this).attr("value");
-      console.log(now);
-      console.log(timeX);
-      input++
-    })
-
-
-
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-    function liveTime(){
-      $('#currentDay').text(dayjs().format('ddd, MMM D, YYYY. h:mm:ss a'));
-    }
-    liveTime();
-    setInterval(liveTime, 1000);
+$(document).ready(function () {
   
+// Declaring the actual time
+  var now = parseInt(dayjs().format('H'));
+
+// Looping to target all the ids. 
+// Pulling saved data from loacl storage and dispaying on screen even after refresh.
+  $('.time-block').each(function(){
+    var el = $(this).attr('id');
+    var timeX = parseInt(el.split('-')[1]);
+    $('#' + el).find('.description').val(localStorage.getItem(el));
+  
+// Defines if it is future, present or past, and applies the required styles.    
+    if(now > timeX){
+      $(this).addClass('past');
+    }
+    else if(now === timeX){
+      $(this).addClass('present');
+    } else{
+      $(this).addClass('future');
+    }
+  })
+  
+// Displaying time and date live on top of the screen. 
+  function liveTime(){
+    $('#currentDay').text(dayjs().format('ddd, MMM D, YYYY. h:mm:ss a'));
+  }
+  
+// Saving textarea input into local storage. 
+  $('.saveBtn').click(function(){
+    var userInput = $(this).siblings('.description').val();
+    localStorage.setItem($(this).parent().attr('id'), userInput);
+  })
+  liveTime();
+  setInterval(liveTime, 1000);
+});
